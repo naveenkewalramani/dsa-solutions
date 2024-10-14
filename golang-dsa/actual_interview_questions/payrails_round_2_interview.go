be1ccf73c625c10f6f7775d2bfe99190a5968692
naveenkewalramani <naveen.kewalramani2903@gmail.com>
2024-10-14 18:20:56 +0530

    chore: Channels,Mutex,WaitGroup

--- golang-dsa/actual_interview_questions/payrails_round_2_interview.go
+++ golang-dsa/actual_interview_questions/payrails_round_2_interview.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// https://leetcode.com/problems/longest-common-prefix/description/
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	fmt.Println(longestCommonPrefix([]string{"dog", "doge", "dogecoin"}))
+}
+
+func longestCommonPrefix(input []string) string {
+	prefix := input[0]
+	for i := 1; i < len(input); i++ {
+		for !strings.HasPrefix(input[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+		}
+		if prefix == "" {
+			return prefix
+		}
+	}
+	return prefix
+}
