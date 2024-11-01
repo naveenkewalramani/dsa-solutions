41582830202a045793c3fc7fd5ccd006be053101
naveenkewalramani <naveen.kewalramani2903@gmail.com>
2024-11-01 23:10:18 +0530

    FirstJavaCode

---
+++ java-dsa/firstProject/src/AddVariables.java
@@ -0,0 +1,10 @@
+public class AddVariables {
+    public static void main(String []args){
+        // Variation 1 - direct variables
+        int a = 20;
+        int b = 10;
+        System.out.println(a + b);
+
+        // TODO: Variation 2 - Take input from reader
+    }
+}
