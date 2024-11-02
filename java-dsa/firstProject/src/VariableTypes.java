65d93584b21d0c776a2d74c635026c7a61d20779
naveenkewalramani <naveen.kewalramani2903@gmail.com>
2024-11-02 12:24:30 +0530

    JavaDatatypes

---
+++ java-dsa/firstProject/src/VariableTypes.java
@@ -0,0 +1,18 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        // types
+        int integerVar = 30303; // primitive predefined
+        float floatVar = 3.14f; // primitive predefined
+        short shortVar = (short) 32; // primitive predefined
+        byte byteVar = (byte) 32; // primitive predefined
+        char charVar = 'c'; // unicode char // primitive predefined
+        String stringVar = "stringVar"; // reference predefined
+        System.out.println(integerVar);
+        System.out.printf("%f\n", floatVar);
+        System.out.println(charVar);
+        System.out.println((int) charVar);
+        System.out.println(stringVar);
+        System.out.println(shortVar);
+        System.out.println(byteVar);
+    }
+}
