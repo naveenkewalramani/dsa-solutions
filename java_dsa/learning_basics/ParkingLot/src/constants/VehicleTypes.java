f3b65bc0b4decb8da69a9f593b52d468c7ec3b31
naveenkewalramani <naveen.kewalramani2903@gmail.com>
2024-11-19 20:13:40 +0530

    MergeJavaBasicsRepo

--- java_dsa/learning_basics/ParkingLot/src/constants/VehicleTypes.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public class VehicleTypes {
-    public final static String vehicleTypeCar = "CAR";
-    public final static String vehicleTypeBike = "BIKE";
-    public final static String vehicleTypeTruck = "TRUCK";
-}
---
+++ java_dsa/learning_basics/PolymorphismImplementation/src/Tree.java
@@ -0,0 +1,14 @@
+public class Tree extends Plant {
+
+	public Tree(String type, Boolean leaves, Boolean flowers) {
+		super(type, flowers, flowers);
+	}
+
+	public void tree_type() {
+		System.out.println(this.type);
+	}
+
+	public void state() {
+		System.out.println("Tree is growing");
+	}
+}
